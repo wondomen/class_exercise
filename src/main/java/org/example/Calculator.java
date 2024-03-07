a4f1d82df6bed51cbb49fdd01003f1ec5ce6de64
wondomen <berhanud@metropolia.fi>
2024-03-07 11:05:44 +0200

    product method added

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,4 +3,7 @@ public class Calculator {
     public int addMe(int a, int b) {
         return a + b;
     }
+    public int productMe(int a, int b) {
+        return a * b;
+    }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -10,4 +10,10 @@ public class CalculatorTest extends TestCase {
         int result1 = calculator.addMe(5, 3);
         assertEquals(8, result1);
     }
+    @Test
+    public void testProductMe() {
+        Calculator calculator = new Calculator();
+        int result2 = calculator.productMe(5, 3);
+        assertEquals(15, result2);
+    }
 }
